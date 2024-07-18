808b8a9d307f28f60a94b0fc1d30de541d1b5d48
BlizardTerrain <92831526+BlizardTerrain@users.noreply.github.com>
2024-07-18 15:38:09 +0930

    Start Function Test
    started Task 2
    - Length, CharacterAt, EquaTo, Append, Prepend, ToLower, ToUpper

--- StringUtilty.cpp
+++ StringUtilty.cpp
@@ -1,11 +1,63 @@
 #include <iostream>
 #include "String.h"
 
+using namespace std;
+
 int main()
 {
 	String greeting("Hello, World!");
 	greeting.WriteToConsole();
-	std::cout << '\n';
+	cout << '\n';
 
-	return 0;
+
+	// Length Function
+	cout << "Length Function: Character count is, " << greeting.Length() << '\n';
+
+	// CharacterAt Function
+	cout << "CharacterAt Function: Your character is, " << greeting.CharacterAt(4) << '\n';
+
+	// EqualTo Function
+	if (greeting.EqualTo("Hello, World!"))
+	{
+		cout << "EqualTo Function: True" << '\n';
+	}
+	else
+	{
+		cout << "EqualTo Function: False" << '\n';
+	}
+
+	// Append Function
+	String appended(greeting);
+	appended.Append(String(" - Appended"));
+	cout << "Append Function: " << appended.CStr() << '\n';
+
+	// Prepend Function
+	String prepended(greeting);
+	prepended.Prepend(String("Prepended - "));
+	cout << "Prepend Function: " << prepended.CStr() << '\n';
+
+	// ToLower Function
+	String lowercased(greeting);
+	lowercased.ToLower();
+	cout << "ToLower Function: " << lowercased.CStr() << '\n';
+
+	// ToUpper Function
+	String uppercased(greeting);
+	uppercased.ToUpper();
+	cout << "ToUpper Function: " << uppercased.CStr() << '\n';
+
+	// Find Function
+
+
+	// Replace Function
+
+	// ReadFromConsole Function
+
+	// WriteToConsole Function
+
+
+		return 0;
+
+
+
 }
