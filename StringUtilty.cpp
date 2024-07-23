11c7fcb04935e980b7f2ddfba6d8fa3cdbbd8cda
DOMPC\domis <domisarb05@gmail.com>
2024-07-24 08:50:57 +0930

    Test Functions  complete

--- StringUtilty.cpp
+++ StringUtilty.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 int main()
 {
+
+	// CStr Function
+	String emptyString;
+	cout << "CStr: " << emptyString.CStr() << '\n';
+
+	//
 	String greeting("Hello, World!");
 	greeting.WriteToConsole();
 	cout << '\n';
-
+	//
 
 	// Length Function
 	cout << "Length Function: Character count is, " << greeting.Length() << '\n';
@@ -47,17 +53,62 @@ int main()
 	cout << "ToUpper Function: " << uppercased.CStr() << '\n';
 
 	// Find Function
-
+	size_t position = greeting.Find(String("World"));
+	if (position != (size_t)-1)
+	{
+		cout << "Find Function: Found at " << position << '\n';
+	}
+	else
+	{
+		cout << "Find Function: Not found" << '\n';
+	}
 
 	// Replace Function
+	String replace(greeting);
+	replace.Replace(String("Hello"), String("Bye"));
+	cout << "Replace Function: " << replace.CStr() << '\n';
 
 	// ReadFromConsole Function
+	cout << "Enter a string: ";
+    String inputString;
+    inputString.ReadFromConsole();
+    cout << "ReadFromConsole Function: " << inputString.CStr() << '\n';
 
 	// WriteToConsole Function
+	cout << "WriteToConsole Function: ";
+    inputString.WriteToConsole();
+    cout << '\n';
 
+	return 0;
 
-		return 0;
+	/*
+    // Operator == Function
+    if (greeting == String("Hello, World!"))
+    {
+        cout << "Operator == Function: True" << '\n';
+    }
+    else
+    {
+        cout << "Operator == Function: False" << '\n';
+    }
 
+    // Operator [] Function
+    cout << "Operator [] Function: Character at position 4 is " << greeting[4] << '\n';
 
+    // Operator = Function
+    String assignment;
+    assignment = greeting;
+    cout << "Operator = Function: " << assignment.CStr() << '\n';
+
+    // Operator != Function
+    if (greeting != String("Hello, Universe!"))
+    {
+        cout << "Operator != Function: True" << '\n';
+    }
+    else
+    {
+        cout << "Operator != Function: False" << '\n';
+    }
+	*/
 
 }
